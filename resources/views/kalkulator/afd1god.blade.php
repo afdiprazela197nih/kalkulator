d425ef9aa517dbcb8b11e8efc55a55cecd3e0d09
afd1got <afdiprazela197@gmail.com>
2024-02-22 14:31:16 +0700

    first commit

---
+++ resources/views/kalkulator/afd1god.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center> kalkulator </center>
+    <center><form name="form1" action="" method="post">
+        <input type="text" name="txt" id=""> 
+
+<br>
+        <input type="button" value="1" onClick="form1.txt.value='1'">     
+        <input type="button" value="2" onClick="form1.txt.value='2'">
+        <input type="button" value="3" onClick="form1.txt.value='3'"> 
+        <input type="button" value="+" onClick="form1.txt.value='+'"> <br>
+        <input type="button" value="4" onClick="form1.txt.value='4'">
+        <input type="button" value="5" onClick="form1.txt.value='5'">
+        <input type="button" value="6" onClick="form1.txt.value='6'">
+        <input type="button" value="-" onClick="form1.txt.value='-'"> <br>
+        <input type="button" value="7" onClick="form1.txt.value='7'">
+        <input type="button" value="8" onClick="form1.txt.value='8'">
+        <input type="button" value="9" onClick="form1.txt.value='9'">
+        <input type="button" value=":" onClick="form1.txt.value=':'"> <br>
+        <input type="button" value="0" onClick="form1.txt.value='0'">
+        <input type="button" value=":)" onClick="form1.txt.value=':)'"> 
+        <input type="button" value="%" onClick="form1.txt.value='%'">
+        <input type="button" value="x" onClick="form1.txt.value='x'"> <br>
+
+        <input type="text2" name="txt2" id=""> 
+        
+
+
+</center>
+</body>
+</html>
